9017fcd527d005ca7681d2bd4d0903fbed886e80
Amrrish1209 <roshanbabubl@gmail.com>
2023-03-19 15:32:23 +0530

    Add: Calculate a wages till a condition of total working hours or day is reached for month

--- src/com/bridgelabz/employeewageproblem/EmployeeWage.java
+++ src/com/bridgelabz/employeewageproblem/EmployeeWage.java
@@ -4,32 +4,33 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 
-		System.out
-				.println("Checking Employee is present or absent if present then we want to calculate the daily wage");
-		int empcheck = Math.floor(Math.random() * 10) % 2 == 1 ? 1 : 0;
-		int fulldayhour = Math.floor(Math.random() * 10) % 2 == 0 ? 4 : 8;
+		System.out.println("Checking Employee is present or absent if present then we want to calculate the daily wage");
 		int wageperhour = 20;
 		int totalworkingdays = 20;
 		int totalworkhour = 0;
+		int maxworkinghour=100;
+		int day=1;
 
-		for (int day = 1; day <= totalworkingdays; day++) {
-
+		while(day<=totalworkingdays && totalworkhour<=maxworkinghour) {
+			int empcheck = Math.floor(Math.random() * 10) % 2 == 1 ? 1 : 0;
+			int dayhour = Math.floor(Math.random() * 10) % 2 == 0 ? 4 : 8;
 			switch (empcheck) {
 			case 1:
 				System.out.println("checking Employee working hour as 4 or 8 hour");
-				if (fulldayhour == 8) {
-					int empwagesalary = wageperhour * fulldayhour;
+				if (dayhour == 8) {
+					int empwagesalary = wageperhour * dayhour;
 					System.out.println("Daily Employee Wage is: " + empwagesalary);
-				} else if (fulldayhour == 4) {
-					int empwageparttimesalary = wageperhour * fulldayhour;
+				} else if (dayhour == 4) {
+					int empwageparttimesalary = wageperhour * dayhour;
 					System.out.println("Part time Employee Wage is: " + empwageparttimesalary);
 				}
-				totalworkhour = totalworkhour + fulldayhour;
+				totalworkhour = totalworkhour + dayhour;
 				break;
 			case 0:
 				System.out.println("Employee is Absent and salary is 0");
 				break;
 			}
+			day++;
 		}
 		int totalsalary = totalworkhour * wageperhour;
 		System.out.println("Total Salary of Employee is: " + totalsalary);
