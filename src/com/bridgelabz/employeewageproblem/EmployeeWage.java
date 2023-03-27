6b4f4b841c5a4065c60d271c52873defdf35b578
Amrrish1209 <roshanbabubl@gmail.com>
2023-03-27 19:23:55 +0530

    Add: Refactor the Code to write a Class Method to Compute Employee Wage

--- src/com/bridgelabz/employeewageproblem/EmployeeWage.java
+++ src/com/bridgelabz/employeewageproblem/EmployeeWage.java
@@ -1,38 +1,37 @@
 package com.bridgelabz.employeewageproblem;
 
 public class EmployeeWage {
+	private static final int wagePerHour = 20;
+    private static final int totalWorkingDays =20;
+    private static final int maxWorkingHour =100;
+    private static int totalWorkHour =0;
 
-	public static void main(String[] args) {
+    
 
-		System.out.println("Checking Employee is present or absent if present then we want to calculate the daily wage");
-		int wageperhour = 20;
-		int totalworkingdays = 20;
-		int totalworkhour = 0;
-		int maxworkinghour=100;
-		int day=1;
+    public static int calculateWage() {
+        int day = 1;
+        while (day <= totalWorkingDays && totalWorkHour <= maxWorkingHour) {
+            int empCheck = Math.floor(Math.random() * 10) % 2 == 1 ? 1 : 0;
+            int dayHour = Math.floor(Math.random() * 10) % 2 == 0 ? 4 : 8;
+            switch (empCheck) {
+                case 1:
+                    if (dayHour == 8) {
+                        totalWorkHour += dayHour;
+                    } else if (dayHour == 4) {
+                        totalWorkHour += dayHour;
+                    }
+                    break;
+                case 0:
+                    break;
+            }
+            day++;
+        }
+        return totalWorkHour * wagePerHour;
+    }
+    public static void main(String[] args) {
+    	
+    	int totalSalary = EmployeeWage.calculateWage();
+    	System.out.println("Total Salary of Employee is: " + totalSalary);
 
-		while(day<=totalworkingdays && totalworkhour<=maxworkinghour) {
-			int empcheck = Math.floor(Math.random() * 10) % 2 == 1 ? 1 : 0;
-			int dayhour = Math.floor(Math.random() * 10) % 2 == 0 ? 4 : 8;
-			switch (empcheck) {
-			case 1:
-				System.out.println("checking Employee working hour as 4 or 8 hour");
-				if (dayhour == 8) {
-					int empwagesalary = wageperhour * dayhour;
-					System.out.println("Daily Employee Wage is: " + empwagesalary);
-				} else if (dayhour == 4) {
-					int empwageparttimesalary = wageperhour * dayhour;
-					System.out.println("Part time Employee Wage is: " + empwageparttimesalary);
-				}
-				totalworkhour = totalworkhour + dayhour;
-				break;
-			case 0:
-				System.out.println("Employee is Absent and salary is 0");
-				break;
-			}
-			day++;
-		}
-		int totalsalary = totalworkhour * wageperhour;
-		System.out.println("Total Salary of Employee is: " + totalsalary);
 	}
 }
