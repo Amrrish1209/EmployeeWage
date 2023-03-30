b85c230c440bf1b6dc301c7a2eced1b9763b951f
Amrrish1209 <roshanbabubl@gmail.com>
2023-03-30 08:48:00 +0530

    Add: Compute Employee Wage for Multiple Company in Procedural Way Using Class Methods

--- src/com/bridgelabz/employeewageproblem/EmployeeWage.java
+++ src/com/bridgelabz/employeewageproblem/EmployeeWage.java
@@ -1,15 +1,10 @@
 package com.bridgelabz.employeewageproblem;
 
 public class EmployeeWage {
-	private static final int wagePerHour = 20;
-    private static final int totalWorkingDays =20;
-    private static final int maxWorkingHour =100;
-    private static int totalWorkHour =0;
 
-    
-
-    public static int calculateWage() {
+    public static int calculateWage(int wagePerHour, int totalWorkingDays , int maxWorkingHour ) {
         int day = 1;
+        int totalWorkHour=0;
         while (day <= totalWorkingDays && totalWorkHour <= maxWorkingHour) {
             int empCheck = Math.floor(Math.random() * 10) % 2 == 1 ? 1 : 0;
             int dayHour = Math.floor(Math.random() * 10) % 2 == 0 ? 4 : 8;
@@ -30,8 +25,11 @@ public class EmployeeWage {
     }
     public static void main(String[] args) {
     	
-    	int totalSalary = EmployeeWage.calculateWage();
-    	System.out.println("Total Salary of Employee is: " + totalSalary);
+    	int companyOneSalary = EmployeeWage.calculateWage(20,20,100);
+    	System.out.println("Total salary for Company 1: $" + companyOneSalary);
+    	
+    	int companySecondSalary = EmployeeWage.calculateWage(20,20,100);
+    	System.out.println("Total salary for Company 2: $"+companySecondSalary);
 
 	}
 }
